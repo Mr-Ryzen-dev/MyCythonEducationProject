b472fbf7d101f62afcb8c9372853313f69b84c34
razedtoa <konstantinkrasnoselskiy@gmail.com>
2025-05-21 23:27:16 +0300

    наше первое мучение

--- main.py
+++ main.py
@@ -3,3 +3,42 @@ import cpp_wrapper
 
 cpp_wrapper.py_relos()
 
+import pygame
+
+pygame.init()
+
+WIDTH,HEIGHT = 1920, 1080
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Супер игра")
+clock = pygame.time.Clock()
+
+background = pygame.image.load("карта.png").convert()
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+player_img = pygame.image.load("ВАЖНО.png").convert_alpha()
+player_img = pygame.transform.smoothscale(player_img, (80, 80))
+player_rect = pygame.Rect(100, 500, 80, 80)
+player_vel_y = 0
+
+
+while True:
+    screen.blit(background, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_rect.x -= SPEED
+    if keys[pygame.K_s]:
+        player_rect.x += SPEED
+    if keys[pygame.K_a]:
+        player_rect.y -= SPEED
+    if keys[pygame.K_d]:
+        player_rect.x += SPEED
+
+
+    pygame.display.flip()
+
+pygame.quit()
